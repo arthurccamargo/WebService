0c3fa4dbf70e1317de0254d396f87e856d2492ea
Arthur <arthurcalazans@gmail.com>
2024-06-19 22:55:50 -0300

    Service layer

--- src/main/java/com/webservices/services/resources/UserResource.java
+++ src/main/java/com/webservices/services/resources/UserResource.java
@@ -1,18 +1,29 @@
 package com.webservices.services.resources;
 
 import com.webservices.services.entities.User;
+import com.webservices.services.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Arthur", "arthur@gmail.com", "999999999", "123456");
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
         return ResponseEntity.ok().body(user);
     }
 }
---
+++ src/main/java/com/webservices/services/services/UserService.java
@@ -0,0 +1,23 @@
+package com.webservices.services.services;
+
+import com.webservices.services.entities.User;
+import com.webservices.services.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        return repository.findById(id).get();
+    }
+}
